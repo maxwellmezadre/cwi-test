acb51104ec7fecd1c5ec76cbddf73aff761054f6
maxwellmezadre <maxwellmezadre@gmail.com>
2025-09-25 23:41:03 -0300

    feat: add routes secured by Passport client credentials and scopes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ExternalController;
+use App\Http\Controllers\HealthController;
+use App\Http\Controllers\UsersDestroyController;
+use App\Http\Controllers\UsersIndexController;
+use App\Http\Controllers\UsersShowController;
+use App\Http\Controllers\UsersStoreController;
+use App\Http\Controllers\UsersUpdateController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Passport\Http\Middleware\EnsureClientIsResourceOwner;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('/health', HealthController::class);
+
+Route::middleware(EnsureClientIsResourceOwner::using('users.read'))
+    ->group(function () {
+        Route::get('/users', UsersIndexController::class);
+        Route::get('/users/{user}', UsersShowController::class);
+    });
+
+Route::middleware(EnsureClientIsResourceOwner::using('users.write'))
+    ->group(function () {
+        Route::post('/users', UsersStoreController::class);
+        Route::put('/users/{user}', UsersUpdateController::class);
+        Route::delete('/users/{user}', UsersDestroyController::class);
+    });
+
+Route::get('/external', ExternalController::class)
+    ->middleware(EnsureClientIsResourceOwner::using('external.read'));
